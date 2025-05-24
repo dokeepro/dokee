c0bbd64b3d29b61518ef99955403746ac3aefff8
Yaroslav <yaroslav7v@gmail.com>
2025-05-24 22:38:03 +0300

    new changes

--- src/sections/type-of-document/TypeOfDocument.tsx
+++ src/sections/type-of-document/TypeOfDocument.tsx
@@ -170,7 +170,7 @@ const TypeOfDocument = () => {
                 formData.append('files', file);
             });
 
-            const response = await fetch('http://localhost:8080/payment/send-data-to-email', {
+            const response = await fetch('https://dokee-be.onrender.com/payment/send-data-to-email', {
                 method: 'POST',
                 body: formData
             });
