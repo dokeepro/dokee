aac2c1d7675921a86a0d13c7a3221a1befa606f8
yaroslav <yaroslav7v@gmail.com>
2026-05-14 16:44:06 +0300

    removed timer

--- src/sections/type-of-document/TypeOfDocument.tsx
+++ src/sections/type-of-document/TypeOfDocument.tsx
@@ -870,7 +870,7 @@ const TypeOfDocument = () => {
 
     const isExpressDisabled = (activeCountry === 'KZ' || activeCountry === 'UA') && isOutsideInterval(8, 14);
     const isFastDisabled = (activeCountry === 'KZ' || activeCountry === 'UA') && isOutsideInterval(8, 16);
-    const isNormalDisabled = false; // temp: always enabled for testing
+    const isNormalDisabled = (activeCountry === 'KZ' || activeCountry === 'UA') && isOutsideInterval(8, 21);
 
     const allAvailableToLanguages: string[] = Array.from(
         new Set(
